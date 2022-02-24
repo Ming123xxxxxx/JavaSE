e4cc458d18510d9105b41b9dc20a2769202562e0
Ming123xxxxxx <2693441186@qq.com>
2022-02-24 15:36:49 +0800

    two commit

--- 常用类/src/Comparable和Comparator/CompaTest.java
+++ 常用类/src/Comparable和Comparator/CompaTest.java
@@ -3,6 +3,7 @@ package Comparable和Comparator;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * @author 温黎明
@@ -21,8 +22,33 @@ public class CompaTest {
     //重写compareTo的规则:x.comparaTo(b) ,返回x和b的差值,可为正可为负也可为0
     @Test
     public void test(){
-        String[] arr = new String[]{"xx","cc","呵呵","aa","dd"};
+        Books[] arr = new Books[4];
+        arr[0]=new Books("Java从入门到入土","Ming",2000);
+        arr[1]=new Books("Mysql从入门到删库","Ling",300);
+        arr[2]=new Books("SpringBoot从入门到精通","xxx",700);
+        arr[3]=new Books("Linux从入门到入牢","Linus",1000);
         Arrays.sort(arr);
+        for(Books b: arr){
+           System.out.println(b);
+        }
+    }
+
+    //Comparator接口的使用:定制排序
+    //背景:当元素的类型没有实现Comparable接口而又不方便修改代码时
+    //或实现了Comparable接口的排序规则不适合当前的操作,那么可以考虑使用Comparator的对象来排序
+    //重写:重写compare(Object o1,Object o2)的方法，比较o1和o2的大小,如果方法返回正整数,
+    //则表示o1大于o2,如果返回0,表示相等,返回负整数.表示o1等于o2
+    @Test
+    public void test1(){
+        String[] arr = new String[]{"CC","DD","AA","RR","呵呵","FF"};
+        //按照字符串从大到小的顺序排序
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return -o1.compareTo(o2);
+            }
+        });
+        //也可简写为Arrays.sort(arr,(e1,e2) -> {return -e1.compareTo(e2)});
         System.out.println(Arrays.toString(arr));
     }
 }
